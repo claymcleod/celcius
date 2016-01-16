32adb46738c30a8a744822a9fcd9ad1ffe44f45e
Clay McLeod <clay.l.mcleod@gmail.com>
2016-01-16 02:19:21 -0500

    generalized confirm_command

--- lib/celcius/terminal_utils.py
+++ lib/celcius/terminal_utils.py
@@ -1,5 +1,12 @@
 from __future__ import print_function
 
+def confirm_command(cmd):
+    print("The command celcius would like to run is:")
+    print()
+    print(cmd)
+    print()
+    return confirm()
+
 def confirm():
     answer = raw_input("Would you like to continue? (Y/n) ")
     while answer.lower() not in ['', 'n', 'y']:
